1f6176a8337ba539a4be1a9418d4b7905fafb1fb
LauraPJ <alu0100703511@ull.edu.es>
2016-07-01 00:13:28 +0100

    Version final de la memoria

---
+++ Memoria/listings/Arrival.java
@@ -0,0 +1,31 @@
+package com.bulletpoint.ull.bulletpoint.busclasses;
+
+public class Arrival {
+
+    private String stopCode;
+    private String stopName;
+    private String destination;
+    private String hour;
+    private String travelId;
+    private String lineNumber;
+    private String minutesForArrival;
+
+
+    public String getStopCode() {
+        return stopCode;
+    }
+
+    public void setStopCode(String stopCode) {
+        this.stopCode = stopCode;
+    }
+
+    public String getStopName() {
+        return stopName;
+    }
+
+    public void setStopName(String stopName) {
+        this.stopName = stopName;
+    }
+
+//...
+}
---
+++ Memoria/listings/BeaconBusStop.java
@@ -0,0 +1,30 @@
+package com.bulletpoint.ull.bulletpoint.beaconInterfaces;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The class BeaconBusStop, storing the information.
+ */
+public class BeaconBusStop {
+
+    private static final Map<String, String> stopsMapId;
+    static {
+        /*Each becon MAC address is associated with the stop ID*/
+        Map<String,String> auxMap = new HashMap<String,String>();
+        //Intercambiador La Laguna
+        auxMap.put("20:C3:8F:F1:AA:11", "2625");
+        //Intercambiador Santa Cruz
+        auxMap.put("20:C3:8F:F1:52:ED", "7140");
+        //Intercambiador Costa Adeje
+        auxMap.put("D4:F5:13:7A:1D:24", "7142");
+
+        stopsMapId = Collections.unmodifiableMap(auxMap);
+    }
+
+    public static String getStopId(String maccAddress){
+        return stopsMapId.get(maccAddress);
+    }
+}
+
